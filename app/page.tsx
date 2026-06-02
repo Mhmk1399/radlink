c14f882591ab01190bcfc37f23616ded354ce4ad
Hossein <141348426+Hossein137900@users.noreply.github.com>
2026-06-02 18:08:13 +0330

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,45 @@
- 
-export default function Home() {
+import {
+  HowItWorksSection,
+  TargetAudienceSection,
+} from "@/components/static/Landing/AudienceAndSteps";
+import { BlocksSection } from "@/components/static/Landing/BlocksSection";
+import {
+  BenefitsSection,
+  CustomizationSection,
+} from "@/components/static/Landing/CustomizationAndBenefits";
+import {
+  DashboardSection,
+  QrCodeSection,
+} from "@/components/static/Landing/DashboardAndQr";
+import {
+  CtaSection,
+  FaqSection,
+  MvpSection,
+} from "@/components/static/Landing/MvpCtaFaq";
+import SmartLandingPage from "@/components/static/Landing/SmartLandingPage";
+import {
+  FeaturesSection,
+  SolutionSection,
+} from "@/components/static/Landing/Solution";
+
+const page = () => {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
+    <main className="min-h-screen bg-linear-to-b from-[#060e1b] via-[#081223] to-[#091828] font-sans text-white antialiased selection:bg-sky-500/30 selection:text-white">
+      <SmartLandingPage />
+      <SolutionSection />
+      <FeaturesSection />
+      <BlocksSection />
+      <TargetAudienceSection />
+      <HowItWorksSection />
+      <DashboardSection />
+      <QrCodeSection />
+      <CustomizationSection />
+      <BenefitsSection />
+      <MvpSection />
+      <CtaSection />
+      <FaqSection />
+    </main>
   );
-}
+};
+
+export default page;
