9e91deb006fe5212fa8760bd4bb6326f7435ff6b
Hossein <141348426+Hossein137900@users.noreply.github.com>
2026-06-03 13:33:27 +0330

    Update index.ts

--- types/index.ts
+++ types/index.ts
@@ -10,7 +10,7 @@
 //
 // ─────────────────────────────────────────────────────────────────
 
- import { ReactNode } from "react";
+import { ReactNode } from "react";
 import { KeyedMutator } from "swr/_internal";
 
 /* ══════════════════════════════════════════════
